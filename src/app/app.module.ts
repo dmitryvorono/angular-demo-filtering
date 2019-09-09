5318707752d2dfc319c4cf2117616aaa6e2c2b47
dmitry voronov <voronov-d88@yandex.ru>
2019-09-09 06:48:53 +0300

    Generate result component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FilterComponent } from './components/filter/filter.component';
+import { ResultComponent } from './components/result/result.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FilterComponent
+    FilterComponent,
+    ResultComponent
   ],
   imports: [
     BrowserModule,
